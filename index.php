76c67fc38ec344f0a0a9f10909fe4523b2d7973a
Jeremy Wall <jeremywall@users.noreply.github.com>
2019-08-16 12:18:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 
-//$url = getenv("URL")."/v1/NoaaExt.json?user=".getenv("DID")."&pass=".getenv("PASSWORD")."&apiToken=".getenv("APITOKEN");
+$url = getenv("URL")."/v1/NoaaExt.json?user=".getenv("DID")."&pass=".getenv("PASSWORD")."&apiToken=".getenv("APITOKEN");
 
-error_log($url);
+//error_log($url);
 
-//step1
-$cSession = curl_init(); 
-//step2
-curl_setopt($cSession,CURLOPT_URL,$url);
-curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
-//step3
-$result=curl_exec($cSession);
-//step4
-curl_close($cSession);
-//step5
+$session = curl_init(); 
+
+curl_setopt($session, CURLOPT_URL, $url);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($session);
+
+curl_close($session);
+
 echo $result;
