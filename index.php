cf69864e568975ff5c60fc9247d8c1f862f8043c
Jeremy Wall <jeremywall@users.noreply.github.com>
2019-08-16 13:24:49 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,19 @@
 
 $url = getenv("URL")."/v1/NoaaExt.json?user=".getenv("DID")."&pass=".getenv("PASSWORD")."&apiToken=".getenv("APITOKEN");
 
-//error_log($url);
+$data = json_decode(file_get_contents($url));
+?>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col"><?= data["temp_f"] ?> &deg; F</div>
+        <div class="col"><?= data["temp_extra_1"] ?> &deg; F</div>
+      </div>
+    </div>
+  </body>
+</html>
 
-echo file_get_contents($url);
-
-/*
-$session = curl_init(); 
-curl_setopt($session, CURLOPT_URL, $url);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($session);
-curl_close($session);
-echo $result;
-*/
